847876293fe35895116e90d1f39c77dd35151016
Alison <alison-cruz@hotmail.com>
2025-02-27 10:05:28 -0300

    fix: corrige erros vercel

--- src/app/api/currently-playing/route.ts
+++ src/app/api/currently-playing/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { prisma } from 'src/lib/prisma'
 import { authOptions } from 'src/lib/auth'
-import { PrismaClient, Prisma } from '@prisma/client'
+import { Prisma } from '@prisma/client'
 
 interface SpotifyArtist {
   name: string
